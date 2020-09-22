fcdc11ba1615299ff75c49abcb6286dabaa672ce
vmvanegas <58665973+vmvanegas@users.noreply.github.com>
2020-09-22 15:20:33 -0500

    Updates

---
+++ precache-manifest.29af04b10cdfc88c65824bce733bc33d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90384252077d8771f2a36ffe778c8961",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "c4e248c94706408ea808",
+    "url": "/curriculum-vitae/static/css/main.ac4e4508.chunk.css"
+  },
+  {
+    "revision": "08f5057f1603227b3558",
+    "url": "/curriculum-vitae/static/js/2.977186fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/curriculum-vitae/static/js/2.977186fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e248c94706408ea808",
+    "url": "/curriculum-vitae/static/js/main.704aed58.chunk.js"
+  },
+  {
+    "revision": "383fbb9de264c5c28f2d",
+    "url": "/curriculum-vitae/static/js/runtime-main.38ee593e.js"
+  }
+]);
